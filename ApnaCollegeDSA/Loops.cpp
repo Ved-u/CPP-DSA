50d9a835b43f365eb09a1fadb355e16e1a4afd2c
Ved-u <vedantutpat084@gmail.com>
2025-10-02 21:24:32 +0530

    streak++

---
+++ ApnaCollegeDSA/Loops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    //for loop 
+    for(int i=1;i<=100;i++){
+        cout<<i<<" ";
+    }
+    //while
+    cout<<endl;
+    int i=1;
+    while(i<=100){
+        cout<<i++<<" ";
+    }
+    // do-while
+    
+    return 0;
+}
