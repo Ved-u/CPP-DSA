8fe79e96671e07177abb622c205060861f2bfb69
Ved-u <vedantutpat084@gmail.com>
2025-10-24 18:23:58 +0530

    streak

--- ApnaCollegeDSA/Loops.cpp
+++ ApnaCollegeDSA/Loops.cpp
@@ -1,17 +1,61 @@
 #include <iostream>
 using namespace std;
-int main(){
+
+int sumDigits(int n)
+{
+    int sum=0;
+    while(n>0)
+    {
+        sum+=n%10;
+        n/=10;
+    }
+    return sum;
+}
+
+int sumOddDigits(int n)
+{
+    int sum=0;
+    while(n>0)
+    {
+        if((n%10)%2!=0)
+            sum+=n%10;
+        n/=10;
+    }
+    return sum;
+}
+
+void loops()
+{
     //for loop 
-    for(int i=1;i<=100;i++){
+    for(int i=1;i<=100;i++)
+    {
         cout<<i<<" ";
     }
     //while
     cout<<endl;
     int i=1;
-    while(i<=100){
+    while(i<=100)
+    {
         cout<<i++<<" ";
     }
+    cout<<endl;
     // do-while
+}
+
+int revNum(int n)
+{
+    int rev=0;
+    while(n>0)
+    {
+        rev=rev*10+n%10;
+        n/=10;
+    }
+   return rev;
+}
+
+int main()
+{
+    cout<<revNum(12345)<<endl;
     
     return 0;
 }
